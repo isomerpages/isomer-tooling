62f99bc2c68ccb89d612a9c8d9e28efc4651ac5d
Harish V <harish@open.gov.sg>
2023-11-17 20:46:48 +0800

    enhance script to count recursively

--- egazette-parser/count_num_of_csv_records_in_directory.js
+++ egazette-parser/count_num_of_csv_records_in_directory.js
@@ -2,31 +2,52 @@ const fs = require("fs");
 const path = require("path");
 const util = require("util");
 
-const directoryPath = "./metadata_mci";
-const outputFilePath = "./metadata_mci/tally.json";
+// const directoryPath = "./metadata_dgs";
+// const outputFilePath = "./metadata_dgs/tally.json";
+
+const directoryPath = "./metadata_mci_formatted";
+const outputFilePath = "./metadata_mci_formatted/tally.json";
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 const readdirAsync = util.promisify(fs.readdir);
+const statAsync = util.promisify(fs.stat);
+
+async function countCsvRecords(filePath) {
+  const data = await readFileAsync(filePath, "utf8");
+  return data.trim().split("\n").length - 1;
+}
+
+async function processDirectory(dirPath) {
+  const entries = await readdirAsync(dirPath);
+  let totalRecords = 0;
+  const fileTallies = {};
+
+  for (let entry of entries) {
+    const fullPath = path.join(dirPath, entry);
+    const stats = await statAsync(fullPath);
+
+    if (stats.isDirectory()) {
+      const { total, tallies } = await processDirectory(fullPath);
+      totalRecords += total;
+      Object.assign(fileTallies, tallies);
+    } else if (path.extname(entry) === ".csv") {
+      const numRecords = await countCsvRecords(fullPath);
+      fileTallies[entry] = numRecords;
+      totalRecords += numRecords;
+    }
+  }
+
+  return { total: totalRecords, tallies: fileTallies };
+}
 
 async function processFiles() {
   try {
-    const files = await readdirAsync(directoryPath);
-    const csvFiles = files.filter((file) => path.extname(file) === ".csv");
-
-    let results = {};
-    let totalRecords = 0;
-
-    for (let file of csvFiles) {
-      const data = await readFileAsync(path.join(directoryPath, file), "utf8");
-      const numRecords = data.trim().split("\n").length - 1;
-      results[file] = numRecords;
-      totalRecords += numRecords;
-    }
+    const { total, tallies } = await processDirectory(directoryPath);
 
     const outputData = {
-      totalRecords,
-      fileTallies: results,
+      totalRecords: total,
+      fileTallies: tallies,
     };
 
     await writeFileAsync(outputFilePath, JSON.stringify(outputData, null, 2));
