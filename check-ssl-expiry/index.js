0fd3f5e5180f3d3296e476b3f1611262dd5961e3
Harish V <harish@open.gov.sg>
2024-06-01 18:04:36 +0800

    enhance script

--- check-ssl-expiry/index.js
+++ check-ssl-expiry/index.js
@@ -1,9 +1,10 @@
 const https = require("https");
 
 // List of domains to check
-const domains = ["example.com", "google.com", "facebook.com"];
+// const domains = ["example.com", "google.com", "facebook.com"];
+const domains = ["google.com", "apple.com"];
 
-// Function to check SSL certificate expiry date
+// Checks if expiry of SSL expiry date is within one month
 function checkSSLCertificate(domain) {
   const options = {
     hostname: domain,
@@ -14,9 +15,24 @@ function checkSSLCertificate(domain) {
 
   const req = https.request(options, (res) => {
     const cert = res.socket.getPeerCertificate();
+
     if (cert.valid_to) {
+      const expiryDate = new Date(cert.valid_to);
+      const currentDate = new Date();
+      const oneMonthLater = new Date(
+        currentDate.setMonth(currentDate.getMonth() + 1)
+      );
+
       console.log(`Domain: ${domain}`);
       console.log(`Expiry Date: ${cert.valid_to}`);
+
+      // Check if the expiry date is within one month from now
+      if (expiryDate < oneMonthLater) {
+        console.log("Warning: Certificate is expiring within one month.");
+      } else {
+        console.log("Certificate is valid for more than one month.");
+      }
+
       console.log("-----------------------------");
     }
   });
@@ -27,7 +43,6 @@ function checkSSLCertificate(domain) {
 
   req.end();
 }
-
 // Iterate over each domain and check their SSL certificate
 domains.forEach((domain) => {
   checkSSLCertificate(domain);
