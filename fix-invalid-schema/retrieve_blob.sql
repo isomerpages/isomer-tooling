5083893cc803247c117b8f1f205cb5f414190407
adriangohjw <adriangohjw@gmail.com>
2024-10-28 18:54:01 +0800

    add search for invalid heading for sql

--- fix-invalid-schema/retrieve_blob.sql
+++ fix-invalid-schema/retrieve_blob.sql
@@ -14,41 +14,30 @@ WITH valid_versions AS (
 			WHERE
 				"draftBlobId" IS NOT NULL)
 ),
-blobs_with_table_header AS (
+blobs_with_invalid_schema AS (
 	SELECT
 		*
 	FROM
 		"Blob"
 	WHERE
+		-- table header with non-paragraph content
 		jsonb_path_exists ("content",
 			'$.** ? (@.type == "tableHeader" && @.content[*].type != "paragraph")')
-),
-blobs_with_empty_prose AS (
-	SELECT
-		*
-	FROM
-		"Blob"
-	WHERE
-		jsonb_path_exists ("content",
+		-- prose with no content
+		OR jsonb_path_exists ("content",
 			'$.** ? (@.type == "prose" && @.content.type() == "array" && @.content.size() == 0)')
+		-- heading with no content
+		OR jsonb_path_exists ("content",
+			'$.** ? (@.type == "heading" && @.attrs.type() == "object" && @.attrs.level == 1)')
 )
-
 -- not great code but it works
 SELECT
 	*
 FROM
-	blobs_with_table_header
+	blobs_with_invalid_schema
 WHERE
 	id IN(
 		SELECT
 			"blobId" FROM valid_versions)
-UNION
-SELECT
-	*
-FROM
-	blobs_with_empty_prose
-WHERE
-	id IN(
-		SELECT
-			"blobId" FROM valid_versions)
-ORDER BY id ASC;
+ORDER BY
+	id ASC;
