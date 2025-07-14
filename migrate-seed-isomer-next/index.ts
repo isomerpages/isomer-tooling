43fe4a2f916a352199accfd43c25b87085d542df
dcshzj <27919917+dcshzj@users.noreply.github.com>
2025-07-14 10:52:09 +0800

    fix: handle spaces in file names

--- migrate-seed-isomer-next/index.ts
+++ migrate-seed-isomer-next/index.ts
@@ -645,27 +645,27 @@ function studioifyContent(
   for (const asset of Object.keys(assetsMap)) {
     newContent = newContent
       .replaceAll(`"${asset}"`, `"${assetsMap[asset]}"`)
-      .replaceAll(`"${asset}"`, `"${assetsMap[asset.replaceAll("%20", " ")]}"`)
+      .replaceAll(`"${asset.replaceAll(" ", "%20")}"`, `"${assetsMap[asset]}"`)
       .replaceAll(`'${asset}'`, `'${assetsMap[asset]}'`)
-      .replaceAll(`'${asset}'`, `'${assetsMap[asset.replaceAll("%20", " ")]}'`);
+      .replaceAll(`'${asset.replaceAll(" ", "%20")}'`, `'${assetsMap[asset]}'`);
   }
 
   for (const page of Object.keys(resourcesMap)) {
     newContent = newContent
       .replaceAll(
-        `"${page}"`,
-        `"[resource:${String(siteId)}:${String(resourcesMap[page].id)}]"`
-      )
-      .replaceAll(
         `"${page}/"`,
         `"[resource:${String(siteId)}:${String(resourcesMap[page].id)}]"`
       )
       .replaceAll(
-        `'${page}'`,
-        `'[resource:${String(siteId)}:${String(resourcesMap[page].id)}]'`
+        `"${page}"`,
+        `"[resource:${String(siteId)}:${String(resourcesMap[page].id)}]"`
       )
       .replaceAll(
         `'${page}/'`,
+        `'[resource:${String(siteId)}:${String(resourcesMap[page].id)}]'`
+      )
+      .replaceAll(
+        `'${page}'`,
         `'[resource:${String(siteId)}:${String(resourcesMap[page].id)}]'`
       );
   }
