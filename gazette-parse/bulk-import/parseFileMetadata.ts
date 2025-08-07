5fce3b7162966f1924f9d8661c90f4a3fc750bc0
adriangohjw <adriangohjw@gmail.com>
2025-08-08 01:30:16 +0800

    add option to skip first row header

--- gazette-parse/bulk-import/parseFileMetadata.ts
+++ gazette-parse/bulk-import/parseFileMetadata.ts
@@ -16,12 +16,14 @@ type CsvFileMetadata = {
 
 interface ParseFileMetadataProps {
 	csvFileName: string;
-	metadataRootFolder: string;
+	csvFileRootFolder: string;
+	skipFirstRowHeaders?: boolean;
 }
 
 export const parseFileMetadata = async ({
 	csvFileName,
-	metadataRootFolder,
+	csvFileRootFolder,
+	skipFirstRowHeaders = false,
 }: ParseFileMetadataProps): Promise<CsvFileMetadata[]> => {
 	const csvFileData = csvFileMapping[csvFileName];
 	if (!csvFileData) throw new Error(`File ${csvFileName} not found in csvFileMapping`);
@@ -29,7 +31,7 @@ export const parseFileMetadata = async ({
 	const isGovernmentGazette = csvFileData.category === Category.GovernmentGazette;
 	const results: CsvFileMetadata[] = [];
 
-	const data = fs.readFileSync(`${metadataRootFolder}/${csvFileName}`, {
+	const data = fs.readFileSync(`${csvFileRootFolder}/${csvFileName}`, {
 		encoding: "utf8",
 	});
 
@@ -37,23 +39,23 @@ export const parseFileMetadata = async ({
 		(value) => value.replace(/^\uFEFF/, "") // remove BOM included in the files
 	);
 
-	for (const row of rows) {
+	for (const row of rows.slice(skipFirstRowHeaders ? 1 : 0)) {
 		const rowData = row.split(",");
 
 		// Note, we are assuming the CSV file has the following columns in a certain order
 		// Do double confirm with the CSV file before running this script
-		const year = rowData[0];
-		const notificationNumber = rowData[1];
+		const year = rowData[0]; // verified (column A)
+		const notificationNumber = rowData[1]; // verified (column B), but some don't have this column -> to clarify
 		const fileName = rowData[2];
-		const publishDate = new Date(rowData[rowData.length - 1]);
+		const publishDate = new Date(rowData[8]); // verified (column I)
 		if (!year || !fileName) throw new Error("invalid csv");
 	
 		if (isGovernmentGazette) {
-			const subCategory = subCategoryMapping[rowData[rowData.length - 2]];
+			const subCategory = subCategoryMapping[rowData[7]]; // verified (column H)
 
 			// Joining all columns from index 3 up to (but not including) the third-to-last column,
 			// handling cases where the title may contain commas.
-			const title = rowData.slice(3, -3).join(",");
+			const title = rowData[3]; // verified (column D)
 
 			if (!subCategory) {
 				console.log(rowData);
@@ -73,7 +75,7 @@ export const parseFileMetadata = async ({
 		} else {
 			// Joining all columns from index 4 up to (but not including) the last column,
 			// handling cases where the title may contain commas.
-			const title = rowData.slice(4, -1).join(",");
+			const title = rowData[3]; // verified (column D)
 
 			results.push({
 				fileName,
